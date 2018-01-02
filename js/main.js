8cc57a57ce2a9df2dc48c29b8899bb6888a4348a
David Halewood <davidnhalewood@hotmail.com>
2018-01-02 11:49:38 +0000

    Adding API call for user

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ $(window).ready(function (){
   $('#search-user').on('click', function (e) {
     let user = $('#user').val()
     
+    $.ajax({
+      url: 'https://api.github.com/users/'+user,
+      data: GithubAccess.data
+    }).done(function(user){
+      console.log(user.name)
+    });
     e.preventDefault()
   })
 });
