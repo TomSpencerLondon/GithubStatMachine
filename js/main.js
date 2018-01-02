3c568d9a273172bea10075aa7c08068d00e3e51e
David Halewood <davidnhalewood@hotmail.com>
2018-01-02 11:57:42 +0000

    Adding API call for each repo for a given user

--- js/main.js
+++ js/main.js
@@ -3,10 +3,18 @@ $(window).ready(function (){
     let user = $('#user').val()
     
     $.ajax({
-      url: 'https://api.github.com/users/'+user,
+      url: 'https://api.github.com/users/'+user+"/repos?per_page=100",
       data: GithubAccess.data
-    }).done(function(user){
-      console.log(user.name)
+    }).done(function(repos){
+      $.each(repos, function(index, repo){
+        let repoName = repo.name
+        $.ajax({
+          url: 'https://api.github.com/repos/'+user+"/"+repoName,
+          data: GithubAccess.data
+        }).done(function(repo){
+          console.log(repo.name)
+        })
+      });
     });
     e.preventDefault()
   })
