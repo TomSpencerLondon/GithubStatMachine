e36f8d2f00973c7bdb68f27d4cd9a827e2b634a4
David Halewood <davidnhalewood@hotmail.com>
2018-01-03 15:24:23 +0000

    Adding ajax request for user activity

--- js/main.js
+++ js/main.js
@@ -12,25 +12,40 @@ $(window).ready(function (){
 
   function getRepoStats() {
     $.ajax({
-      url: 'https://api.github.com/users/'+user+"/repos?per_page=100",
+      url: 'https://api.github.com/users/'+user+'/repos?per_page=100',
       data: githubAccess.data
     }).done(function(repos){
       displayUser();
       $.each(repos, function(index, repo) {
-        getCommitStats(repo)
+        getCommitStats(repo);
+        getActivityStats(repo);
       });
     });
   }
 
   function getCommitStats(repo) {
     $.ajax({
-      url: 'https://api.github.com/repos/'+user+"/"+repo.name+"/stats/participation",
+      url: 'https://api.github.com/repos/'+user+'/'+repo.name+'/stats/participation',
       data: githubAccess.data
     }).done(function(data){
       commitCalculator.addRepoCommits(data);
       displayData();
     });
-  }
+  };
+
+  function getActivityStats(repo) {
+    $.ajax({
+      url: 'https://api.github.com/repos/'+user+'/'+repo.name+'/stats/code_frequency',
+    data: githubAccess.data
+    }).done(function(data){
+      $.each(data, function(index, weeklyActivity){
+        let date = weeklyActivity[0]
+        let additions = weeklyActivity[1]
+        let deletions = weeklyActivity[2]
+        console.log(date, additions, deletions)
+      });
+    });
+  };
 
   function displayUser() {
     $('#user-name').html(`
@@ -42,12 +57,12 @@ $(window).ready(function (){
 
   function displayData() {
     $('#commit-data').html(`
-      <div class="row">
-        <div class="col-md-6">
-          <div id="data-card" class="card">
-            <div class="card-header">
-              Average Commits <span class="badge badge-primary badge-pill">${commitCalculator.returnAverageCommits()}</span>
-              Holiday Commits <span class="badge badge-primary badge-pill">${commitCalculator.returnHolidayCommits()}</span>
+      <div class='row'>
+        <div class='col-md-6'>
+          <div id='data-card' class='card'>
+            <div class='card-header'>
+              Average Commits <span class='badge badge-primary badge-pill'>${commitCalculator.returnAverageCommits()}</span>
+              Holiday Commits <span class='badge badge-primary badge-pill'>${commitCalculator.returnHolidayCommits()}</span>
             </div>
           </div>
         </div>
