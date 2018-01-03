439f172c6057484b6ab4a76093e6751fe54529a1
David Halewood <davidnhalewood@hotmail.com>
2018-01-03 19:51:25 +0000

    Adding githubAccess.js

---
+++ js/githubAccess.js
@@ -0,0 +1,11 @@
+(function (exports) {
+  exports.GithubAccess = function () {
+    const data = {
+      client_id: 'b0b8ceeddc474e0ca482',
+      client_secret: 'd59d74728ee7d58f0fa6b1e8df1f574183ca7696'
+    }
+    return {
+      data: data
+    }
+  };
+})(this);
