b9a3efa7bfa40bb38cae51e7dd2d2394a5a1aa05
David Halewood <davidnhalewood@hotmail.com>
2018-01-11 12:16:12 +0000

    Using dataStub for AcitvityCalculator testing

--- js/spec/activityCalculatorSpec.js
+++ js/spec/activityCalculatorSpec.js
@@ -1,23 +1,11 @@
 describe('ActivityCalculator', function () {
   var activityCalculator = new ActivityCalculator
-  var date = 1510950000
-  var additions = 100
-  var deletions = -10
-  var holidayDate = 1513950000
-  var holidayAdditions = 10
-  var holidayDeletions = -1
-  var weeks = []
-  for (var i = 0; i < 52; i ++) {
-    if (i >= 48) {
-      weeks.push([holidayDate, holidayAdditions, holidayDeletions])
-    } else {
-      weeks.push([date, additions, deletions])
-    }
-  }
-  let repos = [{}]
+  var data = new DataStub
+  var activityData = data.activityData
+  var repos = [{activityData}]
 
   it('Calculates the average activity for a given repo', function () {
-    expect(activityCalculator.repoActivity(weeks)).toEqual({
+    expect(activityCalculator.repoActivity(activityData)).toEqual({
       additions: 91.32075471698113,
       deletions: -9.132075471698114,
       holidayAdditions: 8,
@@ -27,8 +15,8 @@ describe('ActivityCalculator', function () {
 
   it('Calculates the average total activity for a given user', function () {
     let allActivity = []
-    allActivity.push(activityCalculator.repoActivity(weeks))
-    allActivity.push(activityCalculator.repoActivity(weeks))
+    allActivity.push(activityCalculator.repoActivity(activityData))
+    allActivity.push(activityCalculator.repoActivity(activityData))
     expect(activityCalculator.userActivity(allActivity)).toEqual({
       additions: 91,
       deletions: -9,
@@ -36,13 +24,4 @@ describe('ActivityCalculator', function () {
       holidayDeletions: -1
     })
   });
-
-  // it('Calculates the average holiday deletions for a given repo', function () {
-  //   expect(activityCalculator.returnAdditionsHolidayAverage()).toEqual(10)
-  // });
-
-  // it('Calculates the average holiday deletions for a given repo', function () {
-  //   expect(activityCalculator.returnDeletionsHolidayAverage()).toEqual(-1)
-  // });
-
 });
--- js/spec/commitCalculatorSpec.js
+++ js/spec/commitCalculatorSpec.js
@@ -3,10 +3,10 @@ describe('CommitCalculator', function () {
   var data = new DataStub
   var commitData = data.commitData
   it('Calculates the average of a given data set', function () {
-    expect(commitCalculator.returnAverageCommits(commitData)).toEqual(2.25)
+    expect(commitCalculator.returnAverageCommits(commitData)).toEqual(9.81)
   });
 
   it('Calculates the average holiday period of a given data set', function () {
-    expect(commitCalculator.returnHolidayCommits(commitData)).toEqual(1)
+    expect(commitCalculator.returnHolidayCommits(commitData)).toEqual(5)
   });
 });
